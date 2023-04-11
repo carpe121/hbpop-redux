8dd29813f776ce7395436a374f67b6d26d8a07ce
remcr <remcr@channing.harvard.edu>
2023-04-11 16:13:20 -0400

    dfs and vignettes

---
+++ amel.poolseq/R/gb_gbo.R
@@ -0,0 +1,12 @@
+#' GBOld to GB Conversion
+#'
+#' A dataset of conversions between old and new BeeBase Gene IDs
+#'
+#' @format ## `gb_gbold`
+#' A data frame with 2 columns and 14152 rows:
+#' \describe{
+#'	\item{GB}{new Apis mellifera GenBank ID}
+#'	\item{GBold}{old Apis mellifera GenBank ID}
+#' }
+#' @source manually generated
+"gb_gbo"
---
+++ amel.poolseq/R/gb_ncbi.R
@@ -0,0 +1,12 @@
+#' GB to NCBI
+#'
+#' A dataset of conversions between old BeeBase gene IDs and RNA transcripts
+#'
+#' @format ## `gb_ncbi`
+#' A data frame with 2 columns and 22085 rows:
+#' \describe{
+#'	\item{Transcript}{Apis mellifera RNA transcript ID}
+#'	\item{Gene}{old Apis mellifera GenBank ID}
+#' }
+#' @source manually generated
+"gb_ncbi"
